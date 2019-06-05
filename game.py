317ce2977d3d2838223bb3e7a5ab2b1fcc37847e
HarrisonGrubb <harrison.grubb@stern.nyu.edu>
2019-06-05 18:55:58 -0400

    Captured User Input Sucessfully

--- game.py
+++ game.py
@@ -1,3 +1,22 @@
 # game.py
 
 print("Rock, Paper, Scissors, Shoot!")
+
+# CAPTURE User Inputs
+
+user_choice = input("Please choose 'rock', 'paper', or 'scissors' (don't include the quotes)")
+
+print("_____________________________")
+print('YOU CHOSE:', user_choice)
+
+#Validating the inputs
+
+
+
+#GENERATE COMPUTER SELECTION
+
+
+
+#FIND WINNER
+
+#DISPLY FINAL OUTCOMES
